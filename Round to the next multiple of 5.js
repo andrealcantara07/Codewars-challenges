35f418218fb045522aa1b803cffe8f6ae443e99f
Andre Alcantara <andrealcantara0720@gmail.com>
2018-06-30 14:57:54 -0400

    Round to the next multiple of 5.js

---
+++ Round to the next multiple of 5.js
@@ -0,0 +1,15 @@
+/*Given an integer as input, can you round it to the next (meaning, "higher") 5?*/
+
+function roundToNext5(n){
+  // ...
+  
+  return Math.ceil(n/5)*5;
+}
+
+roundToNext5(0);
+roundToNext5(1);
+roundToNext5(3);
+roundToNext5(5);
+roundToNext5(7);
+roundToNext5(39);
+ 
